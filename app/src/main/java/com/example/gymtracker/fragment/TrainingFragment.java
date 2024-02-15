99014b74c316e6255bb1f4f485960a544f1238db
NamNguyen <namy.ngu@gmail.com>
2024-02-15 21:15:26 +1100

    About to do major changes to database - changed set and exerciselog - see lucid charts

--- app/src/main/java/com/example/gymtracker/fragment/TrainingFragment.java
+++ app/src/main/java/com/example/gymtracker/fragment/TrainingFragment.java
@@ -1,4 +1,4 @@
-package com.example.gymtracker;
+package com.example.gymtracker.fragment;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.gymtracker.R;
 
 /**
  * A simple {@link Fragment} subclass.
